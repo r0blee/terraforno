c194b78072b07dc060aa94cb415b5f09e05acd5f
r0blee <39657285+r0blee@users.noreply.github.com>
2026-06-04 20:11:32 +0100

    Create openclaw_directory_created.tf
    Initial commit of open clawcustom analytic

---
+++ modules/jamf-protect/custom-analytics/openclaw_directory_created/openclaw_directory_created.tf
@@ -0,0 +1,19 @@
+# These custom analytics are derived from the examples found at
+# https://github.com/jamf/jamfprotect/tree/main/custom_analytic_detections
+# and are provided as-is.
+# OpenClaw directory created in user's home folder.
+
+resource "jamfprotect_analytic" "open_claw_directory_created" {
+  name        = "OpenClaw directory created"
+  description = "Detection of OpenClaw's hidden directory, created during setup."
+
+  sensor_type = "File System Event"
+  filter      = "$event.path MATCHES \"\\\\/Users\\\\/[^\\\\/]+\\\\/\\\\.openclaw\" AND $event.isNewDirectory == 1"
+
+  categories = ["Visibility"]
+  severity   = "Informational"
+  level      = 0
+  tags = []
+
+  context_item = []
+}
